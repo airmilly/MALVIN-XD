69ee2f7e32df57ae9034ac9f159400ea7ae91631
airmilly <113221761+airmilly@users.noreply.github.com>
2025-08-02 06:25:12 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~4nkU0CrR#O-Oyb8SlTZZ35pi7xT4uRzmi4ABz1MW8dcXLeWsFJIE",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2349134031923", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
